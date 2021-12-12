0bac836c3a6faaa5a583975c3f9c14eaa81d5971
Shree Naath R S <rsshreenaath@gmail.com>
2021-12-12 09:49:45 +0530

    timeout removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,6 @@ import { MovieService } from 'src/movie-store/services/movie.service';
 export class AppComponent {
   title = 'Store';
   constructor(private movieService: MovieService){
-    setTimeout(() => {
-      this.movieService.dispatch();
-    }, 5000);
+    this.movieService.dispatch();
   }
 }
